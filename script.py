6c65e94404e058a9cc619055fd206ca08ae8d8a7
Edgargithub78 <ediazsebas@gmail.com>
2024-10-08 18:24:13 -0500

    Apoyo y Consultas en Seguridad para ONGs en Entornos Complejos

--- script.py
+++ script.py
@@ -1,1 +1,17 @@
-python -m http.server
+import pandas as pd
+
+def cargar_datos(archivo):
+    try:
+        df = pd.read_excel(archivo)
+        return df
+    except Exception as e:
+        print(f"Error al cargar el archivo: {e}")
+
+def mostrar_datos(df):
+    print(df.head())  # Muestra las primeras filas del DataFrame
+
+if __name__ == "__main__":
+    archivo = 'NOTICIAS 2024 COLOMBIA.xlsx'
+    datos = cargar_datos(archivo)
+    if datos is not None:
+        mostrar_datos(datos)
